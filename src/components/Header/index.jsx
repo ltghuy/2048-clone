7f447466c215e0edddf4c7b981c1a68cfc3aec2d
ltghuy <letrangiahuy0507@gmail.com>
2022-07-24 10:43:20 +0700

    Create music features

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { updateGrid, updateGameOver } from '../../redux/gridSlice'
 
 export const Header = () => {
+  const [volumn, setVolumn] = useState(false) 
+  const audioRef = useRef()
   const dispatch = useDispatch()
   const emptyGrid = [
     [0, 0, 0, 0],
@@ -13,6 +15,11 @@ export const Header = () => {
   const resetGrid = () => {
     dispatch(updateGrid(emptyGrid))
     dispatch(updateGameOver(false))
+  }
+
+  const handleVolumn = () => {
+    setVolumn(!volumn)
+    audioRef.current.paused ? audioRef.current.play() : audioRef.current.pause()
   }
 
   return (
@@ -31,7 +38,18 @@ export const Header = () => {
             <p className='font-semibold text-md md:text-xl'>00000</p>
           </div>
         </div>
-        <div className='bg-return w-6 h-6 mt-4 mx-auto cursor-pointer' onClick={resetGrid}/>
+        <div className='flex items-center justify-center mt-4'>
+          <div className='bg-return w-6 h-6 cursor-pointer' onClick={resetGrid}/>
+          <div className='w-6 h-6 cursor-pointer ml-5' onClick={handleVolumn}>
+            <img 
+              className='w-full h-full object-contain'
+              src={volumn ? '/volume-on.svg' : '/volume-off.svg'} 
+              alt="volumn on icon" />
+              <audio ref={audioRef} loop preload="auto">
+                <source src="/audios/game.mp3" type="audio/mpeg" />
+              </audio>
+          </div>
+        </div>
       </div>
     </div>
   )
